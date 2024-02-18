f476afbaf6e57e93f994341fd182fac6cf12f474
harshalbbd <harshal.desale@bbd.co.za>
2024-02-18 23:52:27 +0530

    inserting values in Station table

--- Evstation_Database/migrations/V004_20240218175839__bbdnet10265.sql
+++ Evstation_Database/migrations/V004_20240218175839__bbdnet10265.sql
@@ -12,3 +12,10 @@ VALUES
 ('Olivia', 'Harris', 'olivia@example.com', '7867564534');
 
 
+INSERT INTO Station (station_name, station_email, station_pass, lattitude, longitude, open_time, close_time)
+VALUES
+('Station A', 'stationA@example.com', 'password123', 40.7128, -74.0060, 8, 18),
+('Station B', 'stationB@example.com', 'password456', 34.0522, -118.2437, 9, 19),
+('Station C', 'stationC@example.com', 'password789', 51.5074, -0.1278, 0, 24),
+('Station D', 'stationD@example.com', 'password123', 52.5200, 13.4050, 10, 22),
+('Station E', 'stationE@example.com', 'password456', 48.8566, 2.3522, 0, 24);
