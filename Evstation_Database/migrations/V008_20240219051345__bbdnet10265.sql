08ddb26987860772149d89f6155ea2e4f024e40c
harshalbbd <harshal.desale@bbd.co.za>
2024-02-19 10:46:44 +0530

    adding the view

---
+++ Evstation_Database/migrations/V008_20240219051345__bbdnet10265.sql
@@ -0,0 +1,22 @@
+--create a view for show the user, station information using feedback table	
+    
+    CREATE VIEW User_And_Station_info_using_feedback_View
+AS
+SELECT
+    u.user_id,
+    u.user_Fname,
+    u.user_Lname,
+    u.user_email,
+    f.feedback_id,
+    f.feedback,
+	f.feedback_rating,
+    s.station_id,
+    s.station_name
+FROM
+    Users u
+JOIN
+    feedback f ON u.user_id = f.user_id
+JOIN
+    Station s ON f.station_id = s.station_id;
+
+-- select * from User_And_Station_info_using_feedback_View;
