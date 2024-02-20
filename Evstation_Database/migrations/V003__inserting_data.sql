b3e90a84dfff48b2d699feb0578e8ac4cca6df7a
harshalbbd <harshal.desale@bbd.co.za>
2024-02-20 23:37:04 +0530

    Inserting data into tables

--- Evstation_Database/migrations/V003__inserting_data.sql
+++ Evstation_Database/migrations/V003__inserting_data.sql
@@ -25,6 +25,15 @@ VALUES
 (9, '9012345678'),
 (10, '0123456789');
 
+
+INSERT INTO ChargingStations (Name, EmailID, Password, Latitude, Longitude, OpenTime, CloseTime, RatePerHour) 
+VALUES
+('Station 1', 'station1@example.com', 'password1', 40.7128, -74.0060, '08:00:00', '18:00:00', 10.00),
+('Station 2', 'station2@example.com', 'password2', 34.0522, -118.2437, '09:00:00', '19:00:00', 12.00),
+('Station 3', 'station3@example.com', 'password3', 51.5074, -0.1278, '10:00:00', '20:00:00', 15.00),
+('Station 4', 'station4@example.com', 'password4', 48.8566, 2.3522, '07:00:00', '17:00:00', 11.00),
+('Station 5', 'station5@example.com', 'password5', 41.8781, -87.6298, '08:30:00', '18:30:00', 10.50);
+
 INSERT INTO BookingTimeSlots (StartTime, EndTime) VALUES ('00:00:00', '01:00:00');
 INSERT INTO BookingTimeSlots (StartTime, EndTime) VALUES ('01:00:00', '02:00:00');
 INSERT INTO BookingTimeSlots (StartTime, EndTime) VALUES ('02:00:00', '03:00:00');
@@ -49,15 +58,6 @@ INSERT INTO BookingTimeSlots (StartTime, EndTime) VALUES ('20:00:00', '21:00:00'
 INSERT INTO BookingTimeSlots (StartTime, EndTime) VALUES ('21:00:00', '22:00:00');
 INSERT INTO BookingTimeSlots (StartTime, EndTime) VALUES ('22:00:00', '23:00:00');
 INSERT INTO BookingTimeSlots (StartTime, EndTime) VALUES ('23:00:00', '24:00:00');
-
-INSERT INTO ChargingStations (Name, EmailID, Password, Latitude, Longitude, OpenTime, CloseTime, RatePerHour) 
-VALUES
-('Station 1', 'station1@example.com', 'password1', 40.7128, -74.0060, '08:00:00', '18:00:00', 10.00),
-('Station 2', 'station2@example.com', 'password2', 34.0522, -118.2437, '09:00:00', '19:00:00', 12.00),
-('Station 3', 'station3@example.com', 'password3', 51.5074, -0.1278, '10:00:00', '20:00:00', 15.00),
-('Station 4', 'station4@example.com', 'password4', 48.8566, 2.3522, '07:00:00', '17:00:00', 11.00),
-('Station 5', 'station5@example.com', 'password5', 41.8781, -87.6298, '08:30:00', '18:30:00', 10.50);
-
 
 
 INSERT INTO ChargingStationSlots (StationID)
