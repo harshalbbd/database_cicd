aca2d1f5066f943bf22d7791681de237634118e0
harshalbbd <harshal.desale@bbd.co.za>
2024-02-21 09:24:55 +0530

    Inserting data into tables

--- Evstation_Database/migrations/V008__inserting_data_into_StationFeedbacks.sql
+++ Evstation_Database/migrations/V008__inserting_data_into_StationFeedbacks.sql
@@ -1,15 +1,9 @@
 INSERT INTO StationFeedbacks (UserID, Description, Rating, StationID)
 VALUES
-(31, 'Good service', 4, 1),
-(32, 'Excellent facilities', 5, 2),
-(33, 'Average experience', 3, 3),
-(34, 'Needs improvement', 2, 4),
-(35, 'Very satisfied', 5, 5),
-(36, 'Friendly staff', 4, 4),
-(37, 'Clean environment', 5, 3),
-(38, 'Prompt service', 4, 2),
-(39, 'Convenient location', 5, 1),
-(40, 'Not satisfied', 1, 5);
+(31, 'Good service', 4, 6),
+(32, 'Excellent facilities', 5, 8),
+(35, 'Average experience', 3, 9)
+;
 
 
 
--- Evstation_Database/migrations/V010__inserting_data_into_SlotBookings.sql
+++ Evstation_Database/migrations/V010__inserting_data_into_SlotBookings.sql
@@ -1,4 +1,4 @@
 INSERT INTO SlotsBookings (StatusID, SlotBookingDate, SlotID, StationID, TimeSlotID, UserID) VALUES
-(1, '2024-02-20', 1, 1, 10, 31),
-(1, '2024-02-21', 2, 1, 17, 32),
-(1, '2024-02-23', 7, 4, 9, 35);
+(1, '2024-02-22', 1, 6, 135, 31),
+(1, '2024-02-21', 2, 8, 137, 32),
+(1, '2024-02-23', 7, 9, 131, 35);
